1e7342a51e497b926905551a06e6b0d67ebbc052
prakashpy <52621658+prakashsiv@users.noreply.github.com>
2023-02-23 19:54:17 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  return ('Hello, Prakash')
+  return render_template('home.html')
 
-##print(__name__)##here this print __main__
+
+##print(__name__)##here this will print __main__
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True)
